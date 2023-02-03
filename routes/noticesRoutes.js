008d63e7ffdb59827f8b32d31749ed068d7c2c1e
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2023-02-03 23:44:08 +0100

    changed favNotices route order

--- routes/noticesRoutes.js
+++ routes/noticesRoutes.js
@@ -16,14 +16,14 @@ const { authMiddleware } = require("../middlewares/authMiddleware");
 // create an endpoint to add ads according to the selected category
 router.post("/", authMiddleware, asyncWrapper(addNotice));
 
+// create an endpoint to receive ads of an authorized user created by the same user
+router.get("/favNotices", asyncWrapper(getFavoriteNotices));
+
 // create an endpoint for receiving ads by category
 router.get("/category/:categoryName", asyncWrapper(getNoticesByCategory));
 
 // create an endpoint to receive a single ad
 router.get("/:id", asyncWrapper(getNoticeById));
-
-// create an endpoint to receive ads of an authorized user created by the same user
-router.get("/favNotices", asyncWrapper(getFavoriteNotices));
 
 router.get("/myNotice", authMiddleware, asyncWrapper(getMyNotice));
 //  create an endpoint to delete an authorized user's ad created by the same user
